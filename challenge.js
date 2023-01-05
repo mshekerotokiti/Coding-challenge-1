434fe91d8be18489d972845df2a5a25e382ad318
mshekerotokiti <sharontokiti@gmail.com>
2023-01-05 12:22:22 +0300

    challenge 1

---
+++ challenge.js
@@ -0,0 +1,26 @@
+
+const massMark = 78; 
+const heightMark = 1.69;
+ const BMIMark = 78 / 1.69**2 //mass/(height * height)
+ console.log (BMIMark);
+
+
+ /*massMark = 95;
+ heightMark = 1.88;
+ const BMIMark = 95 / 1.88**2;
+ console.log (BMIMark);*/
+
+ 
+ let johnMass = 92;
+ let johnHeight = 1.95;
+ let BMIJohn = 92 / 1.95**2;
+ console.log (BMIJohn);
+
+
+ /*let johnMass = 85;
+ let johnHeight = 1.76;
+ let BMIJohn = 85 / 1.76**2;
+ console.log (BMIJohn);*/
+
+ const markHigherBMI = BMIMark > BMIJohn ;
+ console.log (markHigherBMI);
